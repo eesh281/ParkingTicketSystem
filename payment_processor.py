4635d1ed865bcd96ff76ad9ac1fa94ab09677cd8
eesh281 <53254126+eesh281@users.noreply.github.com>
2024-04-23 23:33:49 -0400

    Added Description

--- payment_processor.py
+++ payment_processor.py
@@ -1,3 +1,4 @@
 class PaymentProcessor:
+    """This class will process payment with proper info, this method can be modified using different mode of payment like credit, cash, etc"""
     def process_payment(self, amount):
         print(f"Payment processed for ${amount}.")

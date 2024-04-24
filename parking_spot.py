6d67d7a1d64aa3c65ec3d1f820e15caf51b830f4
eesh281 <53254126+eesh281@users.noreply.github.com>
2024-04-23 23:32:39 -0400

    Added Description

--- parking_spot.py
+++ parking_spot.py
@@ -1,4 +1,7 @@
+
 class ParkingSpot:
+    '''This class responsible for checking parking spot is occupied or free spot. 
+spot_id will be used in the constructor to determine if that spot is occupied or free'''
     def __init__(self, spot_id):
         self.spot_id = spot_id
         self.occupied = False
